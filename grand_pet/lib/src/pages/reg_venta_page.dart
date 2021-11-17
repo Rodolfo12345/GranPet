39188a6953078f84cf8e3f59fae9c01a1e6f01f7
Nadrielbh3 <noebravoherrera@gmail.com>
2021-11-17 11:34:25 -0600

    borre los comentarios en reg-venta-page.dart

--- grand_pet/lib/src/pages/reg_venta_page.dart
+++ grand_pet/lib/src/pages/reg_venta_page.dart
@@ -15,68 +15,49 @@ String _muestra = 'Opciones';
 class _reg_venta_PageState extends State<VentasPage> {
   Widget build(BuildContext context) {
     return SafeArea(
-      top: true,
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            children:[
+        top: true,
+        child: Scaffold(
+            body: Center(
+                child: Column(
+                    //mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
               Flexible(
-                child: (
-                  HederVentas()
-                ),
+                child: (HederVentas()),
               ),
               Flexible(
-                child: Row(
-                  children: const [
-                    Text("Productos: ", 
-                      textAlign: TextAlign.center, style: TextStyle(
-                        color: Color(0xfffcbc5c), 
-                        fontSize: 25,
-                      ),
+                  child: Row(
+                children: const [
+                  Text(
+                    "Productos: ",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xfffcbc5c),
+                      fontSize: 25,
                     ),
-                    Spacer(),
-                    /*Flexible(
-                      flex: 2,
-                      child: DropdownButton(
-                        items: _lista.map((String a){
-                          return DropdownMenuItem(
-                              value: a,
-                              child: Text(a));
-                        }).toList(),
-                        onChanged: (String? contenido)=>{
-                          setState((){
-                              _muestra = contenido!;
-                          })
-                        },
-                        hint: Text(_muestra, style: const TextStyle(fontSize: 20),),
-                        )
-                    ),*/
-                  ],
-                )
+                  ),
+                  Spacer(),
+                ],
+              )),
+              const SizedBox(
+                height: 30.0,
               ),
-              /*const SizedBox(height: 30.0,),
-              Lote(),
-              const SizedBox(height: 25.0,),
-              Cantidad(),
-              const SizedBox(height: 25.0,),
-              Guardar(),
-              const SizedBox(height: 25.0,),
-              Fotografia()*/
-              const SizedBox(height: 30.0,)
-,             Premium(),
-              const SizedBox(height: 30.0,),
+              Premium(),
+              const SizedBox(
+                height: 30.0,
+              ),
               Olistico(),
-              const SizedBox(height: 30.0,),
+              const SizedBox(
+                height: 30.0,
+              ),
               SuperPremium(),
-              const SizedBox(height: 30.0,),
+              const SizedBox(
+                height: 30.0,
+              ),
               Guardar(),
-              const SizedBox(height: 30.0,),
+              const SizedBox(
+                height: 30.0,
+              ),
               Fotografia()
-            ]
-          )
-        )
-      )
-    );
+            ]))));
   }
 }
