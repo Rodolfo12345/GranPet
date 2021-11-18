5fdccab600c06412cd14dd9e0ca8ece0dd322698
Rodolfo12345 <rodovargas600@gmail.com>
2021-11-18 11:21:41 -0600

    push

--- grand_pet/lib/src/pages/reg_venta_page.dart
+++ grand_pet/lib/src/pages/reg_venta_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:grand_pet/src/widgest/home_widgest.dart';
-import 'package:grand_pet/src/widgest/login_widgest.dart';
 import 'package:grand_pet/src/widgest/venta_widgets.dart';
 
 class VentasPage extends StatefulWidget {
@@ -21,7 +19,7 @@ class _reg_venta_PageState extends State<VentasPage> {
         body: Center(
           child: Column(
             children: [
-              Flexible(
+              Expanded(
                 child: (HederVentas()),
               ),
               Row(
@@ -30,7 +28,8 @@ class _reg_venta_PageState extends State<VentasPage> {
                 ],
               ),
 
-              Expanded(
+              Flexible(
+                flex: 2,
                 child: ListView(
                 padding: const EdgeInsets.all(8),
                 children: <Widget>[
@@ -39,36 +38,10 @@ class _reg_venta_PageState extends State<VentasPage> {
                     color: Colors.amber[600],
                     child: const Center(child: Text('Producto A')),
                   ),
-                  Container(
-                    height: 50,
-                    color: Colors.amber[500],
-                    child: const Center(child: Text('Producto B')),
-                  ),
-                  Container(
-                    height: 50,
-                    color: Colors.amber[100],
-                    child: const Center(child: Text('Producto C')),
-                  ),
-                                  Container(
-                    height: 50,
-                    color: Colors.amber[600],
-                    child: const Center(child: Text('Producto A')),
-                  ),
-                  Container(
-                    height: 50,
-                    color: Colors.amber[500],
-                    child: const Center(child: Text('Producto B')),
-                  ),
-                  Container(
-                    height: 50,
-                    color: Colors.amber[100],
-                    child: const Center(child: Text('Producto C')),
-                  ),
                   Lote(),
                 ],
                 ),
               ),
-              PaddinHistorial(),
               Row(children: const [
                 Fotografia(),
                 Spacer(),
@@ -95,7 +68,9 @@ class Guardar extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       heroTag: "guardar",
-      onPressed: () => {},
+      onPressed: () => {
+
+      },
       child: const Icon(Icons.save),
       backgroundColor: Colors.blue,
     );
@@ -111,7 +86,10 @@ class Agregar extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       heroTag: "agregar",
-      onPressed: () => {},
+      onPressed: () => {
+        VentanaEmergente(context)
+        
+      },
       child: const Icon(Icons.add),
       backgroundColor: Colors.blue,
     );
@@ -131,6 +109,106 @@ class Fotografia extends StatelessWidget {
       child: const Icon(Icons.add_a_photo),
       backgroundColor: Colors.blue,
       
+    );
+  }
+}
+
+
+VentanaEmergente(BuildContext context) {
+  TextEditingController controladorPersonalizado = TextEditingController();
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Escribe la cantidad'),
+          content: TextField(
+            controller: controladorPersonalizado,
+            keyboardType: TextInputType.number,
+          ),
+          actions: [
+
+            TextButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => const AlertDialog(
+                    title: Text('Cantidad de productos', style: TextStyle(color: Colors.redAccent, ),textAlign: TextAlign.center,),
+                    content: TextField(
+                      
+                      ),
+                    actions: [
+                      
+                    ],
+                  )
+                );
+              },
+              child:const Text('Premium'),
+            ),
+            
+            MaterialButton(
+                elevation: 8.5, child: const Text("Cancelar"), onPressed: () => {}),
+            MaterialButton(
+              elevation: 5.0,
+              child: const Text("Continuar"),
+              onPressed: () => {
+                Navigator.of(context)
+                    .pop(controladorPersonalizado.text.toString()),
+                print('esto es lo que escribes: $controladorPersonalizado')
+              },
+            )
+          ],
+        );
+      });
+}
+
+
+
+/////////////////
+Ventana(BuildContext context) {
+  Widget build(BuildContext context) {
+    final AlertDialog dialog = AlertDialog(
+      title: const Text('Title'),
+      contentPadding: EdgeInsets.zero,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          for (int i = 1; i <= 3; i++)
+            ListTile(
+              title: Text(
+                'option $i',
+                style: Theme.of(context)
+                    .textTheme
+                    .subtitle1
+              ),
+              leading: Radio(
+                value: i,
+                groupValue: 1,
+                onChanged: (_) {},
+              ),
+            ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('ACTION 1'),
+        ),
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('ACTION 2'),
+        ),
+      ],
+    );
+
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            showDialog<void>(context: context, builder: (context) => dialog);
+          },
+          child: const Text("SHOW DIALOG"),
+        ),
+      ),
     );
   }
 }
