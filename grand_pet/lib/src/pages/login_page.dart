e6d6281b5b374eb3f4a244a205213d0b7a590846
Rodolfo12345 <rodovargas600@gmail.com>
2021-11-25 16:57:45 -0600

    acepto comit

--- grand_pet/lib/src/pages/login_page.dart
+++ grand_pet/lib/src/pages/login_page.dart
@@ -1,6 +1,7 @@
-//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:grand_pet/src/widgest/custom_widgets.dart';
+import 'package:grand_pet/ui/input_decorations.dart';
+//import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget{
   static String id = 'login_page';
@@ -14,26 +15,99 @@ class _LoginPageState extends State<LoginPage>{
       top: true,
       child: Scaffold(
         body: Center(
-          child: Column(
-            children:[
-              Flexible(
-                child: (
-                  FondoPrincipal() 
+          child: Center(
+            child: Column(
+              children:[
+                Flexible(
+                  child: (
+                    FondoPrincipal() 
+                  ),
                 ),
-              ),
-              const SizedBox(height: 30.0,),
-              userTextField(),
-              const SizedBox(height: 25.0,),
-              passwordTextField(),
-              const SizedBox(height: 25.0,),
-              bottomLogin(),
-              Flexible(child:InicioSesion())
-            ],
+                const SizedBox(height: 30.0,),
+                userTextField(),
+                const SizedBox(height: 25.0,),
+                passwordTextField(),
+                const SizedBox(height: 25.0,),
+                bottomLogin(),
+                //Flexible(child:InicioSesion())
+                
+                /*ChangeNotifierProvider(
+                  create: ( _ ) => LoginFormProvider(),
+                ),*/
+                /*_LoginForm(),
+                const SizedBox(height: 50),
+                const Text('Crear una nueva cuenta', style: TextStyle(fontSize: 18)),
+                const SizedBox(height: 50),*/
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+/*
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({Key? key}) : super(key: key);
 
-//Validacion
+  @override
+  Widget build(BuildContext context) {
+
+    //final LoginForm = Provider.of<LoginFormProvider>(context);
+
+    return Container(
+      child: Form(
+        //Mantener la referencia del key
+        //key: LoginForm.formkey,
+        
+        child: Column(
+          children: [
+            const SizedBox(height: 15,),
+            TextField(
+              
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: 'rodovargas600@gmail.com',
+                labelText: 'Correo Electronico',
+                prefixIcon: Icons.alternate_email_rounded
+              ),
+            ),
+            const SizedBox(height: 30,),
+
+            TextField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: '*******',
+                labelText: 'Contraseña',
+                prefixIcon: Icons.lock_outlined
+              ),
+            ),
+            const SizedBox(height: 30,),
+
+            MaterialButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                child: const Text(
+                  'Ingresar',
+                  style: TextStyle(color: Colors.white),
+                )
+              ),
+              onPressed: () => {
+                //TODO: Login form
+
+              }
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}*/
