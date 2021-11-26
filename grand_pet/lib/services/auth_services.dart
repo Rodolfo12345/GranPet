2e182b3ce3a8e07e50865add01c36ba4ddff4841
Nadrielbh3 <noebravoherrera@gmail.com>
2021-11-26 17:28:38 -0600

    acepte push de rodo

--- grand_pet/lib/services/auth_services.dart
+++ grand_pet/lib/services/auth_services.dart
@@ -1,39 +1,27 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
-
 class AuthService extends ChangeNotifier {
-
-  final String _baseUrl = 'identitytoolkit.googleapis.com';//url del metodo de autenticacion
-  final String _firebaseToken = 'AIzaSyCr0WHBJIe8lGqoxwbF4A2CtFILXAXHdkI';//token de acceso a la base de datos firebase
-
+  final String _baseUrl =
+      'identitytoolkit.googleapis.com'; //url del metodo de autenticacion
+  final String _firebaseToken =
+      'AIzaSyCr0WHBJIe8lGqoxwbF4A2CtFILXAXHdkI'; //token de acceso a la base de datos firebase
   //agregar usuarios
-  Future<String?> createUser( String email, String password) async
-  {
+  Future<String?> createUser(String email, String password) async {
     //creamos la informacion del post
     final Map<String, dynamic> authData = {
       'email': email,
       'password': password
     };
-
     //creamos el url
-    final url = Uri.https(_baseUrl, '/v1/accounts:signUp', {
-      'key': _firebaseToken
-      }
-    );
-
+    final url =
+        Uri.https(_baseUrl, '/v1/accounts:signUp', {'key': _firebaseToken});
     //disparamos la peticion http //ojo, siempre regresa una respuesta
-    final resp = await http.post(url, body: json.encode (authData));
+    final resp = await http.post(url, body: json.encode(authData));
     //decodificacion de la respuesta
-    final Map<String, dynamic> decodedResp = json.decode( resp.body );
+    final Map<String, dynamic> decodedResp = json.decode(resp.body);
     //verificamos la respuesta
-    print( decodedResp );
-
-
+    print(decodedResp);
   }
-
-
 }
