1d2e3d4d78dd1df213ba1239ef047ad60a613f6d
Rodolfo12345 <rodovargas600@gmail.com>
2021-12-13 15:07:43 -0600

    full

--- grand_pet/lib/services/auth_services.dart
+++ grand_pet/lib/services/auth_services.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -30,30 +29,5 @@ class AuthService extends ChangeNotifier {
     //verificamos la respuesta
     print( decodedResp );
 
-  }
-}
-
-////
-class LoginFormProvider extends ChangeNotifier {
-
-  GlobalKey<FormState> formKey = new GlobalKey<FormState>();
-
-  String email    = '';
-  String password = '';
-
-  bool _isLoading = false;
-  bool get isLoading => _isLoading;
-  
-  set isLoading( bool value ) {
-    _isLoading = value;
-    notifyListeners();
-  }
-  bool isValidForm() {
-
-    print(formKey.currentState?.validate());
-
-    print('$email - $password');
-
-    return formKey.currentState?.validate() ?
   }
 }
